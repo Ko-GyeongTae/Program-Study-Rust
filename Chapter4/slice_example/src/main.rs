621e49e59c5bb818e77bdeaf5b41108ba8aee70a
KoGyeongtae <kokt0203@naver.com>
2021-10-07 21:23:56 +0900

    EDIT

--- Chapter4/slice_example/src/main.rs
+++ Chapter4/slice_example/src/main.rs
@@ -1,3 +1,42 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world");
+    let _word = first_word(&s);
+    s.clear();
+    println!("{}", _word);
+
+    let s = String::from("hello world");
+
+    let _hello = &s[0..5];
+    let _world = &s[6..11];
+
+    let s = String::from("hello");
+
+    let _slice = &s[0..2];
+    let _slice = &s[..2];
+
+    let s = String::from("hello world");
+
+    let len = s.len();
+
+    let _slice = &s[3..len];
+    let _slice = &s[3..];
+
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let _slice = &s[0..len];
+    let _slice = &s[..];
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    
+    s.len()
 }
