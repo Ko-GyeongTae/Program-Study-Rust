245931530c0d5c4eb52509da136e0667eccfad22
KoGyeongtae <kokt0203@naver.com>
2021-10-07 19:59:34 +0900

    EDIT - example

--- Chapter4/ownership/src/main.rs
+++ Chapter4/ownership/src/main.rs
@@ -14,6 +14,10 @@ fn main() {
     let s1 = gives_ownership();
     let s2 = String::from("hello");
     let s3 = takens_and_gives_back(s2);
+
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{}' is {}.", s2, len);
 }
 
 fn append_string() {
@@ -53,4 +57,9 @@ fn gives_ownership() -> String {
 
 fn takens_and_gives_back(a_string: String) -> String {
     a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
