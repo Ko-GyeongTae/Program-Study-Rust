2482ccfa92f9eba1667b399fb7737df368f17a40
KoGyeongtae <kokt0203@naver.com>
2021-10-07 16:11:55 +0900

    EDIT - another_function example

--- Chapter3/another_function/src/main.rs
+++ Chapter3/another_function/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function();
+    another_function1(1);
+
+    let x = five();
+    println!("The value of x is : {}", x);
+    
+    let x = plus_one(five());
+    println!("The value of x is : {}", x);
+} 
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn another_function1(x: i32) {
+    println!("The value of argument x is : {}", x);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
