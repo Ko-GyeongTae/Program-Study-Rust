b00b0a8de1e004e72796ec27c00d9145cce083d1
KoGyeongtae <kokt0203@naver.com>
2021-10-07 20:13:54 +0900

    ADD - pointer example

--- Chapter4/ownership/src/main.rs
+++ Chapter4/ownership/src/main.rs
@@ -11,13 +11,20 @@ fn main() {
     let x = 5;
     makes_copy(x);
 
-    let s1 = gives_ownership();
+    let _s1 = gives_ownership();
     let s2 = String::from("hello");
-    let s3 = takens_and_gives_back(s2);
+    let _s3 = takens_and_gives_back(s2);
 
     let s1 = String::from("hello");
     let (s2, len) = calculate_length(s1);
     println!("The length of '{}' is {}.", s2, len);
+    
+    let s1 = String::from("hello");
+    let len = calculate_length1(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+    change(&mut s);
 }
 
 fn append_string() {
@@ -62,4 +69,12 @@ fn takens_and_gives_back(a_string: String) -> String {
 fn calculate_length(s: String) -> (String, usize) {
     let length = s.len();
     (s, length)
+}
+
+fn calculate_length1(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
