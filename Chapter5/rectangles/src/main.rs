dff710ffc8849e06b03dc81ea1c33d562691e13d
KoGyeongtae <kokt0203@naver.com>
2021-10-08 15:18:20 +0900

    ADD - area1

--- Chapter5/rectangles/src/main.rs
+++ Chapter5/rectangles/src/main.rs
@@ -6,8 +6,19 @@ fn main() {
         "The area of the rectangle is {} square pixels.", 
         area(length1, width1)
     );
+
+    let rect1 = (50, 30);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area1(rect1)
+    );
 }
 
 fn area(length: u32, width: u32) -> u32 {
     length * width
+}
+
+fn area1(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
